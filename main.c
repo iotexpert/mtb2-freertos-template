a3fcfa2edc116751c2428d0002327840e0929753
iotexpert <engineer@iotexpert.com>
2020-09-03 06:22:12 -0400

    updated for ntshell

--- main.c
+++ main.c
@@ -8,8 +8,6 @@
 #include "task.h"
 #include "usrcmd.h"
 
-
-
 volatile int uxTopUsedPriority ;
 TaskHandle_t blinkTaskHandle;
 
@@ -26,33 +24,22 @@ void blink_task(void *arg)
 }
 
 
-
-
 int main(void)
 {
     cy_rslt_t result;
     uxTopUsedPriority = configMAX_PRIORITIES - 1 ; // enable OpenOCD Thread Debugging
 
     /* Initialize the device and board peripherals */
-    result = cybsp_init() ;
-    if (result != CY_RSLT_SUCCESS)
-    {
-        CY_ASSERT(0);
-    }
-
+    cybsp_init() ;
     __enable_irq();
 
     cy_retarget_io_init(CYBSP_DEBUG_UART_TX, CYBSP_DEBUG_UART_RX, CY_RETARGET_IO_BAUDRATE);
-    setvbuf(stdin, NULL, _IONBF, 0);
-    setvbuf(stdout, NULL, _IONBF, 0);
 
     // Stack size in WORDs
     // Idle task = priority 0
     xTaskCreate(blink_task, "blinkTask", configMINIMAL_STACK_SIZE,0 /* args */ ,0 /* priority */, &blinkTaskHandle);
     xTaskCreate(usrcmd_task, "nt shell task", configMINIMAL_STACK_SIZE*2,0 /* args */ ,0 /* priority */, 0);
     vTaskStartScheduler();
-
-
 }
 
 /* [] END OF FILE */
